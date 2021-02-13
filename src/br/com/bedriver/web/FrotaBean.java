a826b6893870dee0116dcbd09fd3319f53862abd
Valtenis Souza <valtenis.souza.1@gmail.com>
2021-02-13 15:29:22 -0300

    possível ajax

--- src/br/com/bedriver/web/FrotaBean.java
+++ src/br/com/bedriver/web/FrotaBean.java
@@ -9,16 +9,37 @@ import javax.faces.bean.RequestScoped;
 import br.com.bedriver.model.FrotaVeiculo;
 import br.com.bedriver.rn.FrotaRN;
 
-
-
 @ManagedBean(name = "frotaBean")
 @RequestScoped
 public class FrotaBean {
+	
 	private List<FrotaVeiculo> lista;
-	
-	
+	private boolean mostraLista = true;
+	private boolean mostraGrafico = false;
+
+	public boolean isMostraLista() {
+		return mostraLista;
+	}
+
+	public boolean isMostraGrafico() {
+		return mostraGrafico;
+	}
+
+	public void setMostraGrafico(boolean mostraGrafico) {
+		this.mostraGrafico = mostraGrafico;
+		this.mostraLista = !mostraGrafico;
+	}
+
+	public void setMostraLista(boolean mostraLista) {
+		this.mostraLista = mostraLista;
+		this.mostraGrafico = !mostraLista;
+	}
+
+	public void setLista(List<FrotaVeiculo> lista) {
+		this.lista = lista;
+	}
+
 	public List<FrotaVeiculo> getLista() {
-		
 		if (this.lista == null) {			
 			FrotaRN frotaRN = new FrotaRN();			
 			this.lista = frotaRN.listar();		}
