179f2f67d08d3214827f1755e897b7a8afc52c7d
Valtenis Souza <valtenis.souza.1@gmail.com>
2021-02-23 18:07:43 -0300

    Abrir/fechar menu

--- src/br/com/bedriver/web/UtilBean.java
+++ src/br/com/bedriver/web/UtilBean.java
@@ -1,35 +1,54 @@
 package br.com.bedriver.web;
 
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.AjaxBehaviorEvent;
 
 @ManagedBean(name = "utilBean")
 @ViewScoped
-public class UtilBean {
-	boolean checked;
+public class UtilBean implements Serializable {
 
+	private static final long serialVersionUID = 1825214734508489734L;
 	
+	boolean errorLogin = false;
+	boolean checked = false;
+	boolean opControl = false;
+
+	public boolean isOpControl() {
+		return opControl;
+	}
+
+	public void setOpControl(boolean opControl) {
+		this.opControl = opControl;
+	}
+
+	public boolean isErrorLogin() {
+		return errorLogin;
+	}
+
+	public void setErrorLogin(boolean errorLogin) {
+		this.errorLogin = errorLogin;
+		this.setOpControl(this.getChecked() || this.errorLogin);
+	}
 
 	public boolean isChecked() {
 		return checked;
 	}
+
 	public boolean getChecked() {
 		return checked;
 	}
+
 	public void setChecked(boolean checked) {
-		
 		this.checked = checked;
+		this.setOpControl(this.getChecked() || this.errorLogin);
 	}
- 
+
 	public void change() {
-		
-		if(isChecked()) {
-			this.checked=false;
-		}else {
-			this.checked = true;
-		}
-		
+		this.setChecked(!this.getChecked());
+		this.setOpControl(this.getChecked() || this.errorLogin);
 	}
 
 	@Override
@@ -37,6 +56,8 @@ public class UtilBean {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + (checked ? 1231 : 1237);
+		result = prime * result + (errorLogin ? 1231 : 1237);
+		result = prime * result + (opControl ? 1231 : 1237);
 		return result;
 	}
 
@@ -51,9 +72,11 @@ public class UtilBean {
 		UtilBean other = (UtilBean) obj;
 		if (checked != other.checked)
 			return false;
+		if (errorLogin != other.errorLogin)
+			return false;
+		if (opControl != other.opControl)
+			return false;
 		return true;
 	}
-
-
-
+	
 }
