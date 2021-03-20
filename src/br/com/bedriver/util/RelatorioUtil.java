8858da767030b11febbd8d601678840e342b0a1a
Valtenis Souza <valtenis.souza.1@gmail.com>
2021-03-20 00:46:39 -0300

    teste relatorio

---
+++ src/br/com/bedriver/util/RelatorioUtil.java
@@ -0,0 +1,136 @@
+package br.com.bedriver.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletContext;
+
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+
+import br.com.bedriver.util.UtilException;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.export.HtmlExporter;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.export.JRXlsExporter;
+import net.sf.jasperreports.engine.export.oasis.JROdtExporter;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleHtmlExporterOutput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+
+public class RelatorioUtil {
+	public static final int RELATORIO_PDF = 1;
+	public static final int RELATORIO_EXCEL = 2;
+	public static final int RELATORIO_HTML = 3;
+	public static final int RELATORIO_PLANILHA_OPEN_OFFICE = 4;
+
+	public StreamedContent geraRelatorio(HashMap parametrosRelatorio, String nomeRelatorioJasper,
+			String nomeRelatorioSaida, int tipoRelatorio) throws UtilException {
+		StreamedContent arquivoRetorno = null;
+		try {
+			Connection conexao = this.getConexao();
+			FacesContext contextoFaces = FacesContext.getCurrentInstance();
+			ExternalContext contextoExterno = contextoFaces.getExternalContext();
+			ServletContext contextoServlet = (ServletContext) contextoExterno.getContext();
+
+			String caminhoRelatorios = contextoServlet.getRealPath("/relatorios");
+			String caminhoArquivoJasper = caminhoRelatorios + File.separator + nomeRelatorioJasper + ".jasper";
+			String caminhoArquivoRelatorio = caminhoRelatorios + File.separator + nomeRelatorioSaida;
+			System.out.println("linha 55");
+			JasperReport relatorioJasper = (JasperReport) JRLoader.loadObjectFromFile(caminhoArquivoJasper);
+
+			JasperPrint impressoraJasper = null;
+			System.out.println("linha 58");
+
+			try {
+				impressoraJasper = JasperFillManager.fillReport(relatorioJasper, parametrosRelatorio, conexao);
+			} catch (Exception e) {
+				return null;
+			}
+
+			System.out.println(Arrays.toString(impressoraJasper.getPropertyNames()));
+			String extensao = null;
+			File arquivoGerado = null;
+
+			switch (tipoRelatorio) {
+			case RelatorioUtil.RELATORIO_PDF:
+				JRPdfExporter pdfExportado = new JRPdfExporter();
+				extensao = "pdf";
+				arquivoGerado = new java.io.File(caminhoArquivoRelatorio + "." + extensao);
+				pdfExportado.setExporterInput(new SimpleExporterInput(impressoraJasper));
+				pdfExportado.setExporterOutput(new SimpleOutputStreamExporterOutput(arquivoGerado));
+				pdfExportado.exportReport();
+				arquivoGerado.deleteOnExit();
+				break;
+			case RelatorioUtil.RELATORIO_HTML:
+				HtmlExporter htmlExportado = new HtmlExporter();
+				extensao = "html";
+				arquivoGerado = new java.io.File(caminhoArquivoRelatorio + "." + extensao);
+				htmlExportado.setExporterInput(new SimpleExporterInput(impressoraJasper));
+				htmlExportado.setExporterOutput(new SimpleHtmlExporterOutput(arquivoGerado));
+				htmlExportado.exportReport();
+				arquivoGerado.deleteOnExit();
+				break;
+			case RelatorioUtil.RELATORIO_EXCEL:
+				JRXlsExporter xlsExportado = new JRXlsExporter();
+				extensao = "xls";
+				arquivoGerado = new java.io.File(caminhoArquivoRelatorio + "." + extensao);
+				xlsExportado.setExporterInput(new SimpleExporterInput(impressoraJasper));
+				xlsExportado.setExporterOutput(new SimpleOutputStreamExporterOutput(arquivoGerado));
+				xlsExportado.exportReport();
+				arquivoGerado.deleteOnExit();
+				break;
+			case RelatorioUtil.RELATORIO_PLANILHA_OPEN_OFFICE:
+				JROdtExporter openExportado = new JROdtExporter();
+				extensao = "ods";
+				arquivoGerado = new java.io.File(caminhoArquivoRelatorio + "." + extensao);
+				openExportado.setExporterInput(new SimpleExporterInput(impressoraJasper));
+				openExportado.setExporterOutput(new SimpleOutputStreamExporterOutput(arquivoGerado));
+				openExportado.exportReport();
+				arquivoGerado.deleteOnExit();
+				break;
+			}
+
+			InputStream conteudoRelatorio = new FileInputStream(arquivoGerado);
+
+			arquivoRetorno = DefaultStreamedContent.builder().contentType("application/" + extensao)
+					.name(nomeRelatorioSaida + "." + extensao).stream(() -> conteudoRelatorio).build();
+
+		} catch (JRException e) {
+			throw new UtilException("NÃo foi possível gerar o relatório.", e);
+		} catch (FileNotFoundException e) {
+			throw new UtilException("Arquivo do relatório não encontrado.", e);
+		}
+
+		return arquivoRetorno;
+	}
+
+	private Connection getConexao() throws UtilException {
+		try {
+			Context initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env/");
+			javax.sql.DataSource ds = (javax.sql.DataSource) envContext.lookup("jdbc/bedriver");
+			return (java.sql.Connection) ds.getConnection();
+		} catch (NamingException e) {
+			throw new UtilException("NÃ£o foi possÃ­vel encontrar o nome da conexÃ£o do banco.", e);
+		} catch (SQLException e) {
+			throw new UtilException("Ocorreu um erro de SQL.", e);
+		}
+	}
+}
--- src/br/com/bedriver/web/SimuladoBean.java
+++ src/br/com/bedriver/web/SimuladoBean.java
@@ -1,16 +1,52 @@
 package br.com.bedriver.web;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.model.StreamedContent;
 
 import br.com.bedriver.model.Simulado;
+import br.com.bedriver.model.Usuario;
 import br.com.bedriver.rn.SimuladoRN;
+import br.com.bedriver.rn.UsuarioRN;
+import br.com.bedriver.util.RelatorioUtil;
+import br.com.bedriver.util.UtilException;
 
 @ManagedBean(name = "simuladoBean")
 @RequestScoped
 public class SimuladoBean {
+
+	private StreamedContent arquivoRetorno;
+	private Date dataInicialRelatorio;
+	private Date dataFinalRelatorio;
+	
+	public Date getDataInicialRelatorio() {
+		return dataInicialRelatorio;
+	}
+
+	public void setDataInicialRelatorio(Date dataInicialRelatorio) {
+		this.dataInicialRelatorio = dataInicialRelatorio;
+	}
+
+	public Date getDataFinalRelatorio() {
+		return dataFinalRelatorio;
+	}
+
+	public void setDataFinalRelatorio(Date dataFinalRelatorio) {
+		this.dataFinalRelatorio = dataFinalRelatorio;
+	}
+
+	public void setArquivoRetorno(StreamedContent arquivoRetorno) {
+		this.arquivoRetorno = arquivoRetorno;
+	}
 
 	private Simulado simulado = new Simulado();
 	private List<Simulado> lista;
@@ -49,6 +85,60 @@ public class SimuladoBean {
 	public String setSimuladoEscolhido(Simulado simuladoEscolhido) {
 		this.simuladoEscolhido = simuladoEscolhido;
 		return "/public/pergunta.xhtml";
+	}
+	
+	public StreamedContent getArquivoRetorno() {
+		System.out.println("entrou");
+		FacesContext context = FacesContext.getCurrentInstance();
+		
+		Usuario u = getUsuarioLogado();
+		String[] splitNome = u.getNome().split(" ");
+		String nomeUser = (splitNome.length > 0) ? splitNome[0] : u.getNome();
+		String nomeRelatorioJasper = "simuladorelatorio";
+		String nomeRelatorioSaida = nomeUser.toLowerCase() + "_simulados";
+		
+		RelatorioUtil relatorioUtil = new RelatorioUtil();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+		
+		String usuarioemail = u.getEmail();
+		String datainicio = sdf.format(dataInicialRelatorio);
+		String datafim = sdf.format(dataFinalRelatorio);
+		
+		System.out.println("usuarioemail: " + usuarioemail);
+		System.out.println("datainicio: " + datainicio);
+		System.out.println("datafim: " + datafim);
+		
+		HashMap parametrosRelatorio = new HashMap();
+		parametrosRelatorio.put("usuarioemail", usuarioemail);
+		parametrosRelatorio.put("datainicio", datainicio);
+		parametrosRelatorio.put("datafim", datafim);
+
+		try {
+			this.arquivoRetorno = relatorioUtil.geraRelatorio(parametrosRelatorio, nomeRelatorioJasper,
+					nomeRelatorioSaida, RelatorioUtil.RELATORIO_PDF);
+			if(this.arquivoRetorno == null) throw new NullPointerException();
+		} catch (UtilException e) {
+			context.addMessage(null, new FacesMessage(e.getMessage()));
+			return null;
+		} catch(NullPointerException e) {
+			System.out.println("BATEU NULL AE OH");
+			return null;
+		}
+		System.out.println("saiu");
+		return this.arquivoRetorno;
+	}
+	
+	public Usuario getUsuarioLogado() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		ExternalContext external = context.getExternalContext();
+		String login = external.getRemoteUser();
+		if (login != null) {
+			UsuarioRN usuarioRN = new UsuarioRN();
+			Usuario usuario = usuarioRN.buscarPorLogin(login);
+			return usuario;
+		}
+		return null;
 	}
 
 }
