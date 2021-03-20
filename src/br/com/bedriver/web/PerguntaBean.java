674a920a9f4170798f41dd113ef0f075e499d338
mrpapaia <djoser.dr@gmail.com>
2021-03-20 00:29:08 -0300

    Funfou

--- src/br/com/bedriver/web/PerguntaBean.java
+++ src/br/com/bedriver/web/PerguntaBean.java
@@ -3,14 +3,19 @@ package br.com.bedriver.web;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
+import javax.faces.event.PhaseId;
 import javax.faces.event.ValueChangeEvent;
 
+
 import org.primefaces.component.datatable.DataTable;
+import org.primefaces.component.selectoneradio.SelectOneRadio;
 import org.primefaces.event.data.PageEvent;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
@@ -25,6 +30,7 @@ public class PerguntaBean {
 	private List<Pergunta> lista;
 	private List<String> alternativas;
 	private int indexPergunta;
+	private String aux;
 
 	public List<Pergunta> getLista() {
 
@@ -59,29 +65,21 @@ public class PerguntaBean {
 		
 		String[] alternativa = aux.split(";");
 
-		alternativas = Arrays.asList(alternativa);
+		System.out.println("ads");
 
-		return alternativas;
+		return  Arrays.asList(alternativa);
 	}
 
 	public void setAlternativas(List<String> alternativas) {
 		this.alternativas = alternativas;
 	}
 
-	public void gambiarra() {
-		UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
-		UIComponent component = viewRoot.findComponent("cadastro").findComponent("pergunta");
-
-		DataTable dt = (DataTable) component;
-		System.out.println(dt.getRowIndex());
-	}
-
-	public void onPage(PageEvent event) {
-		setPergunta(event.getPage());
-	}
 	
-	public void handleChange(ValueChangeEvent event){
-	    System.out.println("New value: " + event.getNewValue());
-	}
+
+	 public void getTeste(AjaxBehaviorEvent event){
+		 SelectOneRadio sor = (SelectOneRadio)event.getComponent();
+		 System.out.println(sor.getSubmittedValue().toString());
+		    
+		}
 
 }
