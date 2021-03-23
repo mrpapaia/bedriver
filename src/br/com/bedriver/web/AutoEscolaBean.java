914fb9a194351b58870f197d07ae76d66c536db0
mrpapaia <djoser.dr@gmail.com>
2021-03-23 08:38:38 -0300

    Mapa funcionando

--- src/br/com/bedriver/web/AutoEscolaBean.java
+++ src/br/com/bedriver/web/AutoEscolaBean.java
@@ -5,13 +5,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.primefaces.event.map.OverlaySelectEvent;
 import org.primefaces.model.map.DefaultMapModel;
 import org.primefaces.model.map.LatLng;
 import org.primefaces.model.map.MapModel;
@@ -29,9 +32,8 @@ import com.google.maps.*;
 import br.com.bedriver.gmap.AutoEscola;
 import br.com.bedriver.gmap.Result;
 
-
 @ManagedBean(name = "autoescolaBean")
-@RequestScoped
+@ViewScoped
 public class AutoEscolaBean implements Serializable {
 	/**
 	 * 
@@ -41,49 +43,55 @@ public class AutoEscolaBean implements Serializable {
 	private MapModel simpleModel;
 	private LatLng coordIni;
 	private String cidade;
-	private String address;
+	private String fotoRef;
 	private String geo = "49.817491999999992, 15.4729620";
+	private Marker marker;
+
 	private static final Logger logger = LogManager.getLogger(EstadoBean.class);
 
-	public AutoEscola getAutoEscola() {
+	public void getAutoEscola(String nome) {
 
 		RestTemplate restTemplate = new RestTemplate();
-		System.out.println("getAuto: " + cidade);
+
 		rateResponse = restTemplate.getForObject(
-				"https://maps.googleapis.com/maps/api/place/textsearch/json?input=Auto%Escolas%em%" + cidade
+				"https://maps.googleapis.com/maps/api/place/textsearch/json?input=Auto%Escolas%em%" + nome
 						+ "&inputtype=textquery&fields=name,geometry&key=AIzaSyBVvrkOP2LWY3QF4noU1I081zQelL0JXGA",
 				AutoEscola.class);
 
 		logger.info(
 				"Requisi��o REST realizada em https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome");
 
-		return rateResponse;
-	}
-	
-	@PostConstruct
-	public void init() {
 		simpleModel = new DefaultMapModel();
-		System.out.println("INICIO");
-		getAutoEscola();
 		geo = rateResponse.getResults().get(0).getGeometry().getLocation().getLat() + ","
 				+ rateResponse.getResults().get(0).getGeometry().getLocation().getLng();
 
 		for (Result autoEscola : rateResponse.getResults()) {
 			LatLng coord = new LatLng(autoEscola.getGeometry().getLocation().getLat(),
 					autoEscola.getGeometry().getLocation().getLng());
-			simpleModel.addOverlay(new Marker(coord, autoEscola.getName()));
+
+			simpleModel.addOverlay(new Marker(coord, autoEscola.getName(), autoEscola));
+			// simpleModel.addOverlay(new Marker(coord, autoEscola.getName()));
 		}
-		// Shared coordinates
-		/*
-		 * LatLng coord1 = new LatLng(36.879466, 30.667648); LatLng coord2 = new
-		 * LatLng(36.883707, 30.689216); LatLng coord3 = new LatLng(36.879703,
-		 * 30.706707); LatLng coord4 = new LatLng(36.885233, 30.702323);
-		 * 
-		 * //Basic marker simpleModel.addOverlay(new Marker(coord1, "Konyaalti"));
-		 * simpleModel.addOverlay(new Marker(coord2, "Ataturk Parki"));
-		 * simpleModel.addOverlay(new Marker(coord3, "Karaalioglu Parki"));
-		 * simpleModel.addOverlay(new Marker(coord4, "Kaleici"));
-		 */
+		// return rateResponse;
+	}
+
+	@PostConstruct
+	public void init() {
+		simpleModel = new DefaultMapModel();
+		
+		getAutoEscola("ipameri");
+		geo = rateResponse.getResults().get(0).getGeometry().getLocation().getLat() + ","
+				+ rateResponse.getResults().get(0).getGeometry().getLocation().getLng();
+
+		for (Result autoEscola : rateResponse.getResults()) {
+			LatLng coord = new LatLng(autoEscola.getGeometry().getLocation().getLat(),
+					autoEscola.getGeometry().getLocation().getLng());
+			Marker a = new Marker(coord, autoEscola.getName());
+
+			simpleModel.addOverlay(a);
+		}
+		
+		 
 	}
 
 	public MapModel getSimpleModel() {
@@ -99,34 +107,47 @@ public class AutoEscolaBean implements Serializable {
 	}
 
 	public String getCidade() {
-		System.out.println("get: "+ cidade);
+		
 		return cidade;
 	}
 
 	public void setCidade(String cidade) {
-		System.out.println("set: "+ cidade);
+		
 		this.cidade = cidade;
 	}
 
-	public void updateMapCenter(ActionEvent ae) {
-		System.out.println("click: " + cidade);
-	}
 
-	public String getAddress() {
-		return address;
-	}
 
-	public void setAddress(String address) {
-		this.address = address;
+	public void onMarkerSelect(OverlaySelectEvent event) {
+ 
+		marker = (Marker) event.getOverlay();
+		fotoRef = ((Result) marker.getData()).getPhotos().get(0).getPhoto_reference();
+		//FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Marker Selected", marker.getTitle()));
+
 	}
 
 	public String getGeo() {
-		System.out.println("GEO: " + geo);
 		return geo;
 	}
 
 	public void setGeo(String geo) {
-
 		this.geo = geo;
 	}
+
+	public Marker getMarker() {
+		return marker;
+	}
+
+	public void setMarker(Marker marker) {
+		this.marker = marker;
+	}
+
+	public String getFotoRef() {
+		return fotoRef;
+	}
+
+	public void setFotoRef(String fotoRef) {
+		this.fotoRef = fotoRef;
+	}
+
 }
